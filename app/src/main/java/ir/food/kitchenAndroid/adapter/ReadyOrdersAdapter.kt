7d31a3457a583603a744945ee90e738ecc2ed3a6
fatemeh <fatimanoori1998@gmail.com>
2022-01-03 14:44:02 +0330

    add pack button in ready order items

--- app/src/main/java/ir/food/kitchenAndroid/adapter/ReadyOrdersAdapter.kt
+++ app/src/main/java/ir/food/kitchenAndroid/adapter/ReadyOrdersAdapter.kt
@@ -7,18 +7,26 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ViewFlipper
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import ir.food.kitchenAndroid.R
+import ir.food.kitchenAndroid.app.EndPoints
 import ir.food.kitchenAndroid.app.MyApplication
 import ir.food.kitchenAndroid.databinding.ItemReadyOrdersBinding
 import ir.food.kitchenAndroid.dialog.CallDialog
+import ir.food.kitchenAndroid.dialog.GeneralDialog
 import ir.food.kitchenAndroid.helper.DateHelper
 import ir.food.kitchenAndroid.helper.StringHelper
 import ir.food.kitchenAndroid.helper.TypefaceUtil
 import ir.food.kitchenAndroid.model.CartModel
 import ir.food.kitchenAndroid.model.ReadyOrdersModel
+import ir.food.kitchenAndroid.okHttp.RequestHelper
+import ir.food.kitchenAndroid.push.AvaCrashReporter
+import org.json.JSONException
+import org.json.JSONObject
 
 class ReadyOrdersAdapter(list: ArrayList<ReadyOrdersModel>) :
     RecyclerView.Adapter<ReadyOrdersAdapter.ViewHolder>() {
@@ -27,6 +35,9 @@ class ReadyOrdersAdapter(list: ArrayList<ReadyOrdersModel>) :
 
     lateinit var cartModels: ArrayList<CartModel>
     lateinit var adapter: CartAdapter
+    lateinit var vfSetReady: ViewFlipper
+    lateinit var btnPack: Button
+    var orderId: String = ""
 
     class ViewHolder(val binding: ItemReadyOrdersBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -71,6 +82,20 @@ class ReadyOrdersAdapter(list: ArrayList<ReadyOrdersModel>) :
                 color = R.color.delivery
             }
         }
+        var tapTwice = false
+        holder.binding.btnPacked.setOnClickListener {
+            vfSetReady = holder.binding.vfSetReady
+            orderId = model.id
+            if (tapTwice) {
+                setPack()
+            } else {
+                tapTwice = true
+                MyApplication.handler.postDelayed({ tapTwice = false }, 500)
+            }
+        }
+
+        holder.binding.txtTotalPrice.text = "kdjlksj"
+
         holder.binding.imgStatus.setImageResource(icon)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             val header =
@@ -107,5 +132,56 @@ class ReadyOrdersAdapter(list: ArrayList<ReadyOrdersModel>) :
     override fun getItemCount(): Int {
         return models.size
     }
+
+    private fun setPack() {
+        vfSetReady.displayedChild = 1
+        RequestHelper.builder(EndPoints.READY)
+            .addParam("orderId", orderId)
+            .listener(packCallBack)
+            .put()
+    }
+
+    private val packCallBack: RequestHelper.Callback =
+        object : RequestHelper.Callback() {
+            override fun onResponse(reCall: Runnable?, vararg args: Any?) {
+                MyApplication.handler.post {
+                    try {
+                        val response = JSONObject(args[0].toString())
+                        val success = response.getBoolean("success")
+                        val message = response.getString("message")
+                        if (success) {
+                            btnPack.setBackgroundResource(R.drawable.bg_green)
+                            btnPack.text = "بسته بندی شده"
+                        } else {
+                            vfSetReady.displayedChild = 0
+                            GeneralDialog()
+                                .message(message)
+                                .secondButton("تلاش مجدد") { setPack() }
+                                .show()
+                        }
+                    } catch (e: JSONException) {
+                        vfSetReady.displayedChild = 0
+                        GeneralDialog()
+                            .message("خطایی پیش آمده دوباره امتحان کنید.")
+                            .secondButton("تلاش مجدد") { setPack() }
+                            .show()
+                        e.printStackTrace()
+                        AvaCrashReporter.send(e, "NotReadyOrderFragment class, readyCallBack")
+                    }
+                }
+            }
+
+            override fun onFailure(reCall: Runnable?, e: java.lang.Exception?) {
+                MyApplication.handler.post {
+                    vfSetReady.displayedChild = 0
+                    GeneralDialog()
+                        .message("خطایی پیش آمده دوباره امتحان کنید.")
+                        .secondButton("تلاش مجدد") { setPack() }
+                        .show()
+                }
+                super.onFailure(reCall, e)
+            }
+        }
+
 
 }
